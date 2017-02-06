650915834bd085fcc1aa1053fbba60342c098988
tidburyj <tidburyj@coventry.ac.uk>
2017-02-06 18:50:34 +0000

    Test suite set up using Jasmine spec written for index.js

---
+++ spec/index-spec.js
@@ -0,0 +1,22 @@
+'use strict'
+const request = require('request')
+const base_url = 'http://localhost:8080/test'
+const index = require('../index.js')
+const SuccessCode = 200
+const ErrCode = 500
+
+describe('Test Suite for INDEX.JS', function() {
+	it('returns statusCode 200', function(done) {
+		request.get(base_url, function(error, response){
+			expect(response.statusCode).toBe(SuccessCode)
+			done()
+		})
+	})
+	it('Returns correct Api Status', function(done) {
+		request.get(base_url, function(error, response, body){
+			const status = JSON.parse(body)
+			expect(status.ApiStatus).toBe('Online')
+			done()
+		})
+	})
+})
