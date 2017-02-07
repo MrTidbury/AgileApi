1476e48c121b0c8633b1e7f87bda0f9b7f18f1b4
tidburyj <tidburyj@coventry.ac.uk>
2017-02-07 21:19:00 +0000

    updated port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 80
 const database = require('./modules/database')
 
 app.get('/test', function(req,res) {
