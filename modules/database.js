52a2f8466ebec3bd575fe5a9b74c23812adca201
tidburyj <tidburyj@coventry.ac.uk>
2017-02-07 02:06:07 +0000

    Added test suite for Datbase, as well as an endpoint for testing Database Connection

--- modules/database.js
+++ modules/database.js
@@ -1,5 +1,6 @@
 'use strict'
 const mysql = require('mysql')
+const ErrCode = 500
 const connection = mysql.createConnection({
 	host: '91.238.160.3',
 	user: 'voidreal_apitest',
@@ -7,14 +8,23 @@ const connection = mysql.createConnection({
 	database: 'voidreal_rankme'
 })
 
+connection.connect()
 exports.testdb = function testdb(req, res){
-	connection.connect()
+
+	connection.query('SELECT * from servers', function(err) {
+		if (!err)
+		 res.json({DatabaseConnectionStatus: 'Succsessful'})
+		else
+    res.status(ErrCode).json({DatabaseConnectionStatus: err})
+	})
+
+}
+exports.printdb = function printdb(req, res){
 	connection.query('SELECT * from servers', function(err, rows) {
 		if (!err)
 			res.send(rows)
 		else
     console.log(err)
-	})
+	 })
 
-	connection.end()
 }
