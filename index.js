756f6e7dafd3282ddfe15597f6d2fac464694b40
tidburyj <tidburyj@coventry.ac.uk>
2017-02-08 21:11:30 +0000

    changed port to comply with dokku expectations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const app = express()
-const port = 80
+const port = 5000
 const database = require('./modules/database')
 
 app.get('/test', function(req,res) {
