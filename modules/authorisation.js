cbfec106c39ceb98bf74ea8d7b6ce95329964c4d
tidburyj <tidburyj@coventry.ac.uk>
2017-02-17 00:54:33 +0000

    Added a add user function TODO - email validation

--- modules/authorisation.js
+++ modules/authorisation.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql')
 const passwordHash = require('password-hash')
 const uuidV4 = require('uuid')
-const mailer = require('nodemailer')
+//const mailer = require('nodemailer')
 const ErrCode = 500
 const AuthErrCode = 403
 const SuccsessCode = 200
@@ -12,6 +12,9 @@ const connection = mysql.createConnection({
 	password: 'h720DR262lr',
 	database: 'mydb'
 })
+const isEmpty = function(obj) {
+	return Object.keys(obj).length === 0
+}
 
 connection.connect()
 exports.adduser = function adduser(req, res){
@@ -21,26 +24,32 @@ exports.adduser = function adduser(req, res){
 	const parts=auth.split(/:/)
 	const email=parts[0]
 	const password=parts[1]
-
-	if (password){
-    	const hashedPassword = passwordHash.generate(password)
-	}
-
+	const hashedPassword = passwordHash.generate(password)
 	const course = req.headers.course
 	const name = req.headers.name
-	const position = req.headers.position
+	const valcode = uuidV4()
 
-  	connection.query('SELECT * FROM users WHERE `Email` = '+email , function(err, rows) {
+	console.log(email)
+  	connection.query('SELECT * FROM users WHERE email = "'+email+'"' , function(err, rows) {
 		if (!err) {
-			if(rows){
-				res.status(AuthErrCode).json({error: 'User with email'+email+'already exists'})
+			if(!isEmpty(rows)){
+				res.status(AuthErrCode).json({error: 'User with email '+email+' already exists'})
 			}			else {
-				connection.query('//INSERT USER INTO User DATABASE' , function(err, rows) {
+
+				const student = {name: name, email: email, course: course}
+
+				connection.query('INSERT INTO students SET ?', student, function(err, rows) {
 					if (!err) {
-            //also insert the rest of the data into student/lecturers database
-            // send validation email.
+						const user = {email: email, passwordhash: hashedPassword,user_id: rows.insertId, staff: '0', validationcode: valcode, validated: '0'}
+
+						connection.query('INSERT INTO users SET ?', user, function(err, rows) {
+							if (!err){
+								res.status(SuccsessCode).json({status: 'sucsess', studentid: rows.insertId})
+							}							else {
+		      			res.status(ErrCode).json({error: err})
+							}
+						})
 					}					else{
-						console.log (err)
       			res.status(ErrCode).json({error: err})
 					}
 				})
