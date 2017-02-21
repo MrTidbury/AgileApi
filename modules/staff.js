1bae83aca52254a8834bc8500419923d4e6f2a4b
tidburyj <tidburyj@coventry.ac.uk>
2017-02-21 01:15:06 +0000

    updated json return for staff search

--- modules/staff.js
+++ modules/staff.js
@@ -21,7 +21,7 @@ exports.search = function search(req,res){
 		connection.query('SELECT l.name,l.lec_id,l.title FROM skill_link sl, skills s, lecturers l WHERE sl.skill_id=s.skill_id AND sl.lec_id=l.lec_id AND s.skill_name LIKE \'%'+term+'%\'', function(err, rows) {
       //name id title
   		if (!err){
-  			res.json(rows)
+  			res.json({results: rows})
   		}		else
       res.status(ErrCode).json({error: err})
 			console.log(err)
@@ -29,7 +29,7 @@ exports.search = function search(req,res){
 	}	else if(tag === 'language'){
 		connection.query('SELECT l.name,l.lec_id,l.title FROM languages_link, languages s, lecturers l WHERE sl.lang_id=s.lang_id AND sl.lec_id=l.lec_id AND s.lang_name LIKE \'%'+term+'%\'', function(err, rows) {
 			if(!err){
-				res.json(rows)
+				res.json({results: rows})
 			}			else{
 				res.status(ErrCode).json({error: err})
   			console.log(err)
@@ -38,7 +38,7 @@ exports.search = function search(req,res){
 	}	else if(tag === 'name'){
 		connection.query('SELECT name, title, lec_id FROM lecturers WHERE name LIKE \'%'+term+'%\'', function(err, rows){
 			if(!err){
-				res.json(rows)
+				res.json({results: rows})
 			}			else{
 				res.status(ErrCode).json({error: err})
   			console.log(err)
@@ -47,7 +47,7 @@ exports.search = function search(req,res){
 	}	else if(tag === 'speaks'){
 		connection.query('SELECT name, title, lec_id FROM lecturers WHERE speaks LIKE \'%'+term+'%\'', function(err, rows){
 			if(!err){
-				res.json(rows)
+				res.json({resutls: rows})
 			}			else{
 				res.status(ErrCode).json({error: err})
 				console.log(err)
